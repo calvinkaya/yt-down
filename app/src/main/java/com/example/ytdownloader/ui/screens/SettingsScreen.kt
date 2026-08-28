80e18218c16ff571aaba24f7bdef638baa6ee016
calvinkaya <calvinemrekaya06@gmail.com>
2026-08-28 20:18:36 +0200

    Update SettingsScreen.kt

--- app/src/main/java/com/example/ytdownloader/ui/screens/SettingsScreen.kt
+++ app/src/main/java/com/example/ytdownloader/ui/screens/SettingsScreen.kt
@@ -1,6 +1,6 @@
 package com.example.ytdownloader.ui.screens
 
-import androidx.compose.foundation.layout.Alignment
+import androidx.compose.ui.Alignment
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
